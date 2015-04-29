0853ae410391e657909084a7fcdb2bae5cb2d3fc
temblorosa <amandalynne.p@gmail.com>
2015-04-28 22:43:59 -0400

    updating code

--- xml2crf.py
+++ xml2crf.py
@@ -1,6 +1,29 @@
-import nltk, os, string
+"""Extract features from files for CRF training format"""
+
+import nltk, os, string, sys
 from bs4 import BeautifulSoup
+from itertools import izip
 from xml.etree.ElementTree import ElementTree
 
 
-for xml_file
+def pairwise(iterable):
+    """Helps iterate over pairs of items"""
+    a = iter(iterable)
+    return izip(a, a)
+
+def extract_features_from_files(directory):
+    """Extract features from corpus of files"""
+    side_effect_list = []
+    for annotated, raw in pairwise(os.listdir(directory)):
+        if annotated.endswith('extents.xml'):
+            annotated_tree = ElementTree().parse(directory+annotated)
+            tags = annotated_tree.find('TAGS')
+            for effect in tags.findall('SIDE_EFFECT'):
+                print effect.attrib['text']
+    
+
+
+
+if __name__ == "__main__":
+    directory = sys.argv[1]
+    extract_features_from_files(directory)
